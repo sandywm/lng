5fc598ba89d4a08918b8647eb092d629f438cea7
sandywm <270826057@qq.com>
2019-12-11 17:36:46 +0800

    添加 Signed-off-by: sandywm <270826057@qq.com>

--- src/main/java/com/lng/controller/LngController.java
+++ src/main/java/com/lng/controller/LngController.java
@@ -1,5 +1,8 @@
 package com.lng.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lng.pojo.GasFactory;
 import com.lng.pojo.LngPriceDetail;
-import com.lng.pojo.SuperDep;
-import com.lng.pojo.SuperUser;
 import com.lng.service.GasFactoryService;
 import com.lng.service.LngPriceDetailService;
-import com.lng.service.LngService;
 import com.lng.tools.CommonTools;
 import com.lng.tools.CurrentTime;
-import com.lng.tools.MD5;
 import com.lng.util.Constants;
 import com.lng.util.GenericResponse;
 import com.lng.util.ResponseFormat;
@@ -41,24 +40,66 @@ public class LngController {
 	@PostMapping("addInitBatchData")
 	@ApiOperation(value = "初始批量增加记录",notes = "初始批量增加记录用")
 	@ApiResponses({@ApiResponse(code = 1000, message = "服务器错误"),
-		@ApiResponse(code = 50003, message = "数据已存在"),
 		@ApiResponse(code = 70001, message = "无权限访问")
-	})
-	@ApiImplicitParams({
-		@ApiImplicitParam(name = "gfId", value = "液厂编号(多个用逗号隔开)", required = true)
 	})
 	public GenericResponse addInitBatchData(HttpServletRequest request) {
 		Integer status = 200;
-		String gfId = CommonTools.getFinalStr("gfId", request);
 		try {
 			if(CommonTools.checkAuthorization(CommonTools.getLoginUserId(request), Constants.ADD_LNG_PRICE)) {
-				if(!gfId.equals("")) {
+				String crrentTime = CurrentTime.getCurrentTime();
+				List<GasFactory> gfList = gfs.listInfoByOpt("", "", "", "", "", "", -1);
+				List<LngPriceDetail> list = new ArrayList<LngPriceDetail>();
+				for(GasFactory gf : gfList) {
+					LngPriceDetail lpd = new LngPriceDetail(gf, 0, crrentTime, "", crrentTime);
+					list.add(lpd);
+				}
+				lpds.saveBatch(list);
+			}else {
+				status = 70001;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			status = 1000;
+		}
+		return ResponseFormat.retParam(status, "");
+	}
+	
+	@PostMapping("addBatchLngData")
+	@ApiOperation(value = "增加记录",notes = "手动批量增加记录用")
+	@ApiResponses({@ApiResponse(code = 1000, message = "服务器错误"),
+		@ApiResponse(code = 70001, message = "无权限访问"),
+		@ApiResponse(code = 10002, message = "参数为空")
+	})
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "gfId", value = "液厂编号"),
+		@ApiImplicitParam(name = "price", value = "价格"),
+		@ApiImplicitParam(name = "priceTime", value = "价格时间")
+	})
+	public GenericResponse addBatchLngData(HttpServletRequest request) {
+		String gfId = CommonTools.getFinalStr("gfId", request);
+		String price = CommonTools.getFinalStr("price", request);
+		String priceTime = CommonTools.getFinalStr("priceTime", request);
+		Integer status = 200;
+		try {
+			if(CommonTools.checkAuthorization(CommonTools.getLoginUserId(request), Constants.ADD_LNG_PRICE)) {
+				if(!gfId.equals("") && !price.equals("")) {
 					String[] gfIdArr = gfId.split(",");
+					String[] priceArr = price.split(",");
+					String currentTime = CurrentTime.getCurrentTime();
+					List<LngPriceDetail> list = new ArrayList<LngPriceDetail>();
 					for(int i = 0 ; i < gfIdArr.length ; i++) {
-						GasFactory gs = gfs.getEntityById(gfIdArr[i]);
-//						LngPriceDetail lpd = new LngPriceDetail(gs, 0, CurrentTime.getCurrentTime(), "", String addTime);
+						GasFactory gf = gfs.getEntityById(gfIdArr[i]);
+						Integer lngPrice = 0;
+						if(!priceArr[i].equals("")) {
+							lngPrice = Integer.parseInt(priceArr[i]);
+						}
+						LngPriceDetail lpd = new LngPriceDetail(gf, lngPrice, priceTime+currentTime.substring(10), "", currentTime);
+						list.add(lpd);
 					}
-//					lpds.saveBatch(lpdList);
+					lpds.saveBatch(list);
+				}else {
+					status = 10002;
 				}
 			}else {
 				status = 70001;
