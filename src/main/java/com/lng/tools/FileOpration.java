7914c1b142f105561cadb19ab3c6990bf3b6c1de
sandywm <270826057@qq.com>
2020-03-08 22:12:04 +0800

    修改等比压缩方法 Signed-off-by: sandywm <270826057@qq.com>

--- src/main/java/com/lng/tools/FileOpration.java
+++ src/main/java/com/lng/tools/FileOpration.java
@@ -38,6 +38,7 @@ public class FileOpration {
 				 fs.close();
 				 return true;
 			 }
+			 file = null;
 		 }catch (Exception e) {            
 			 System.out.println("复制单个文件操作出错");            
 			 e.printStackTrace();       
@@ -54,6 +55,7 @@ public class FileOpration {
 		if(!file.exists()){
 			flag = true; 
 		}
+		file = null;
 		return flag;
 	}
 	//删除文件夹里面的所有文件和文件夹
@@ -76,6 +78,7 @@ public class FileOpration {
 			}
 			file.delete();  
 		}
+		file = null;
 		return true;
 	}
 	
@@ -92,6 +95,7 @@ public class FileOpration {
 		if(file.exists()){
 			return CommonTools.convertInputNumber((file.length() / 1024.0)) + "KB";
 		}
+		file = null;
 		return "";
 	}
 	
@@ -113,26 +117,52 @@ public class FileOpration {
 		 File file_1 = new File(url);
 	     BufferedImage bi = ImageIO.read(file_1);
 	     Integer width_old = bi.getWidth();//原始尺寸
+	     Integer height_old = bi.getHeight();
 	     //用Image里的方法对图片进行等比压缩,只要宽和高其一值为负,则以正的那个值为最大边进行等比压缩
-	     Image image = bi.getScaledInstance((int)(width_old * rate), -1,Image.SCALE_AREA_AVERAGING);
-	     int height = image.getHeight(null);
-	     int width = image.getWidth(null);
-	     //以新的高和宽构造一个新的缓存图片
-	     BufferedImage bi1 = new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB);
-	     Graphics g = bi1.getGraphics();
-	     //在新的缓存图片中画图
-	     g.drawImage(image, 0, 0, null);
-	     //构造IO流输出到文件
-	     File file_2 = new File(newUrl);
-	     FileOutputStream fos = new FileOutputStream(file_2);
-	     ImageIO.write(bi1, formatName, fos);
-	     fos.flush();
-	     fos.close();
-	     file_1 = null;//file置为null是为了告诉gc此块内存可以回收
-	     file_2 = null;//file置为null是为了告诉gc此块内存可以回收
+//	     Image image = bi.getScaledInstance((int)(width_old * rate), -1,Image.SCALE_AREA_AVERAGING);
+//	     int height = image.getHeight(null);
+//	     int width = image.getWidth(null);
+//	     //以新的高和宽构造一个新的缓存图片
+//	     BufferedImage bi1 = new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB);
+//	     Graphics g = bi1.getGraphics();
+//	     //在新的缓存图片中画图
+//	     g.drawImage(image, 0, 0, null);
+//	     //构造IO流输出到文件
+//	     File file_2 = new File(newUrl);
+//	     FileOutputStream fos = new FileOutputStream(file_2);
+//	     ImageIO.write(bi1, formatName, fos);
+//	     fos.flush();
+//	     fos.close();
+//	     file_1 = null;//file置为null是为了告诉gc此块内存可以回收
+//	     file_2 = null;//file置为null是为了告诉gc此块内存可以回收
+	     
+	     //下列方法效率高
+	     Integer newWidth = (int)(width_old * rate);
+	     Integer newHeight = (int)(height_old * rate);
+	     FileOutputStream out = null;
+         try {
+             BufferedImage tag = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+             tag.getGraphics().drawImage(bi, 0, 0, newWidth, newHeight, null);
+             out = new FileOutputStream(newUrl);
+             ImageIO.write(tag, "jpg", out);
+             out.flush();
+             out.close();
+             tag.flush();
+             tag = null;
+             bi.flush();
+             bi = null;
+         } catch (Exception e) {
+             e.printStackTrace();
+         } finally {
+             if (out != null) {
+                 out.close();
+             }
+             file_1 = null;//file置为null是为了告诉gc此块内存可以回收
+             bi = null;
+         }
 	     long endTime=System.currentTimeMillis();
-			float excTime=(float)(endTime-startTime)/1000;
-			System.out.println("等比压缩耗费时间--"+excTime);
+		 float excTime=(float)(endTime-startTime)/1000;
+		 System.out.println("等比压缩耗费时间--"+excTime);
 	 }
 	 
 	 /**
@@ -159,12 +189,14 @@ public class FileOpration {
 	 }
 	 
 	 public static void main(String[] args) throws Exception{
-		 for(int i = 1 ; i <= 5 ; i++){
-			 long systime = new Date().getTime();//当前系统时间
-			 FileOpration.makeImage("d:/12.jpg", i/10.0, "d:/12"+i+".jpg", "JPEG");
-			 long oldtime = new Date().getTime();//相比较的时间
-			 Long time = (oldtime - systime);//相差毫秒数
-			 System.out.println("按照"+(i*10)+"%等比压缩图片耗费时间："+time+"毫秒");
+		 for(int i = 1 ; i <= 9 ; i++){
+//			 long systime = new Date().getTime();//当前系统时间
+//			 FileOpration.makeImage("d:/12.jpg", i/10.0, "d:/12"+i+".jpg", "JPEG");
+//			 long oldtime = new Date().getTime();//相比较的时间
+//			 Long time = (oldtime - systime);//相差毫秒数
+//			 System.out.println("按照"+(i*10)+"%等比压缩图片耗费时间："+time+"毫秒");
+			 FileOpration.makeImage("d:/IMG_20200228_172347.jpg", 0.5, "d:/1a1a1aaaa"+i+".jpg", "JPEG");
 		 }
+		 
 	 }
 }
